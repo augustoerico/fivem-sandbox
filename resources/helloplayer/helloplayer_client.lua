93f0a8a6ba89bdd707bceb58e62c7eec51a51513
Erico Silva <augusto.ericosilva@gmail.com>
2018-11-03 11:01:05 -0400

    Make character visible

--- resources/helloplayer/helloplayer_client.lua
+++ resources/helloplayer/helloplayer_client.lua
@@ -12,18 +12,35 @@ end
 function LoadFreemodeModelAsync ()
     Citizen.CreateThread(function ()
         DebugInChat('[ loading  model ...')
-        model = GetHashKey("mp_f_freemode_01")
+        model = GetHashKey("mp_m_freemode_01")
         while (not HasModelLoaded(model)) do
             -- Busy wait...
             RequestModel(model)
             Citizen.Wait(1)
         end
-        SetPlayerModel(GetPlayerIndex(), model)
-        DebugInChat('... ... ... ...done]')
+        local playerIndex = GetPlayerIndex()
+        SetPlayerModel(playerIndex, model)
+        SetPedDefaultOutfit()
+        DebugInChat('playerIndex: ' .. playerIndex)
+        DebugInChat('... ... ... ..done ]')
     end)
 end
 
-AddEventHandler('playerSpawned', function (spawn)
-    DebugInChat('playerSpawned event in "helloplayer_client"')
-    LoadFreemodeModelAsync()
-end)
+function SetPedDefaultOutfit ()
+    local playerPed = GetPlayerPed(-1)
+    DebugInChat('playerPed: ' .. playerPed)
+    DebugInChat('[ setting outfit ...')
+    SetPedComponentVariation(playerPed, 0, 0, 0, 2) --Face
+    SetPedComponentVariation(playerPed, 2, 11, 4, 2) --Hair 
+    SetPedComponentVariation(playerPed, 4, 1, 5, 2) -- Pantalon
+    SetPedComponentVariation(playerPed, 6, 1, 0, 2) -- Shoes
+    SetPedComponentVariation(playerPed, 11, 7, 2, 2) -- Jacket
+    DebugInChat('[ ... ... .. .done ]')
+end
+
+AddEventHandler('playerSpawned',
+    function (spawn)
+        DebugInChat('playerSpawned event in "helloplayer_client"')
+        LoadFreemodeModelAsync()
+    end
+)
