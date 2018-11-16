51d415f190d0ae15051a4d54011e6fdab9903d39
Erico Silva <augusto.ericosilva@gmail.com>
2018-11-15 19:46:35 -0500

    Add comments on getting SteamId

--- resources/helloplayer/helloplayer_server.lua
+++ resources/helloplayer/helloplayer_server.lua
@@ -1,4 +1,10 @@
-AddEventHandler("playerConnecting", function (name, setEventCancelMessage, deferrals, source)
+AddEventHandler("playerConnecting", function (name, setEventCancelMessage, deferrals)
     print("name: " .. tostring(name))
     print("source: " .. tostring(source))
+    for k, v in pairs(GetPlayerIdentifiers(source))
+    do
+        -- [0] is the SteamId
+        print(tostring(k) .. ' = ' .. tostring(v))
+    end
+
 end)
